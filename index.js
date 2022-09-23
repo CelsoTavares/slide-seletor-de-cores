56e382730ba389415b991a7707b472008548f0f2
CelsoTavares <antoniocelsojr@yahoo.com.br>
2022-09-23 20:06:52 -0300

    selector

---
+++ index.js
@@ -0,0 +1,42 @@
+let change1 = document.querySelector('.color1');
+let change2 = document.querySelector('.color2');
+let change3 = document.querySelector('.color3');
+let change4 = document.querySelector('.color4');
+let change5 = document.querySelector('.color5');
+let change6 = document.querySelector('.color6');
+let change7 = document.querySelector('.color7');
+let change8 = document.querySelector('.color8');
+let change9 = document.querySelector('.color9');
+let change10 = document.querySelector('.color10');
+
+change1.addEventListener("click", () => {
+      document.body.style.backgroundColor = "black"
+    })
+change2.addEventListener("click", () => {
+      document.body.style.backgroundColor = "#00FFFF"
+    })
+change3.addEventListener("click", () => {
+      document.body.style.backgroundColor = "#FF0066"
+    })
+change4.addEventListener("click", () => {
+      document.body.style.backgroundColor = "gray"
+    })
+change5.addEventListener("click", () => {
+      document.body.style.backgroundColor = "#000099"
+    })
+change6.addEventListener("click", () => {
+      document.body.style.backgroundColor = "#FF9900"
+    })
+change7.addEventListener("click", () => {
+      document.body.style.backgroundColor = "yellow"
+    })
+change8.addEventListener("click", () => {
+      document.body.style.backgroundColor = "blue"
+    })
+change9.addEventListener("click", () => {
+      document.body.style.backgroundColor = "green"
+    })
+change10.addEventListener("click", () => {
+      document.body.style.backgroundColor = "red"
+    })
+
