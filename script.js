5814189f3d80d3a3de52bbaa32f7d005039c41d6
CelsoTavares <antoniocelsojr@yahoo.com.br>
2022-09-27 17:04:39 -0300

    slides

---
+++ script.js
@@ -0,0 +1,72 @@
+let change1 = document.querySelector('.color1');
+let change2 = document.querySelector('.color2');
+let change3 = document.querySelector('.color3');
+let change4 = document.querySelector('.color4');
+let change5 = document.querySelector('.color5');
+let change6 = document.querySelector('.color6');
+let change7 = document.querySelector('.color7');
+let change8 = document.querySelector('.color8');
+let change9 = document.querySelector('.color9');
+let change10 = document.querySelector('.color10');
+let img1 = document.querySelector('.img');
+
+change1.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#ff8080"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/welcome1.jpg)'
+})
+
+change2.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#ffbf80"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/morning2.jpg)'
+})
+
+change3.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#ffe680"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/every3.jpg)'
+})
+
+change4.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#80bfff"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/yes4.jpg)'
+})
+
+change5.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#8080ff"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/message5.png)'
+})
+
+change6.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#80ffff"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/agua6.jpg)'
+})
+
+change7.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#80ffdf"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/creative7.jpg)'
+})
+
+change8.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#ff80ff"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/fight8.jpg)'
+})
+
+change9.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#99e699"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/folha9.png)'
+})
+
+change10.addEventListener("click", () => {
+  document.body.style.backgroundColor = "#df9f9f"
+  img1.style.height = "80vh"
+  img1.style.backgroundImage = 'url(./img/thank10.jpg)'
+})
+
