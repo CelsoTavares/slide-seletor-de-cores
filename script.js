fdf0ab751ef3f48a5fc450825542a2f4ea255b3d
CelsoTavares <antoniocelsojr@yahoo.com.br>
2022-09-28 22:50:16 -0300

    pronto

--- script.js
+++ script.js
@@ -8,65 +8,87 @@ let change7 = document.querySelector('.color7');
 let change8 = document.querySelector('.color8');
 let change9 = document.querySelector('.color9');
 let change10 = document.querySelector('.color10');
-let img1 = document.querySelector('.img');
+let img = document.querySelector('.first-img');
+let allImages = document.querySelectorAll('img')
 
-change1.addEventListener("click", () => {
+ative()
+let color = [
+ "#ff8080",
+ "#ffbf80",
+ "#ffe680",
+ "#80bfff",
+ "#8080ff",
+ "#80ffff",
+ "#80ffdf",
+ "#ff80ff",
+ "#99e699",
+ "#df9f9f"
+]
+
+let counter = 0;
+
+function ative(){
+  setInterval( ()=> {
+  counter++
+  
+  if(counter > 9){
+  counter = 0
+   }
+ 
+  document.body.style.backgroundColor = color[counter]
+  
+  var percentage = `-${counter}00%`
+  img.style.marginLeft = percentage
+  }, 3000)
+}
+
+change1.addEventListener("click", () =>{ 
   document.body.style.backgroundColor = "#ff8080"
-  img1.style.backgroundImage = 'url(./img/welcome1.jpg)'
-  img1.style.height = "80vh"
+  img.style.marginLeft = '-100%'
 })
 
 change2.addEventListener("click", () => {
   document.body.style.backgroundColor = "#ffbf80"
-  img1.style.backgroundImage = 'url(./img/morning2.jpg)'
-  img1.style.height = "80vh"
+  img.style.marginLeft = '-100%'
 })
 
 change3.addEventListener("click", () => {
   document.body.style.backgroundColor = "#ffe680"
-  img1.style.backgroundImage = 'url(./img/every3.jpg)'
-  img1.style.height = "80vh"
+  img.style.marginLeft = '-200%'
 })
 
 change4.addEventListener("click", () => {
-  document.body.style.backgroundColor = "#80bfff"
-  img1.style.backgroundImage = 'url(./img/yes4.jpg)'
-  img1.style.height = "80vh"
+ document.body.style.backgroundColor = "#80bfff"
+ img.style.marginLeft = '-300%'
 })
 
 change5.addEventListener("click", () => {
   document.body.style.backgroundColor = "#8080ff"
-  img1.style.backgroundImage = 'url(./img/message5.png)'
-  img1.style.height = "80vh"
+  img.style.marginLeft = '-400%'
 })
 
 change6.addEventListener("click", () => {
   document.body.style.backgroundColor = "#80ffff"
-  img1.style.backgroundImage = 'url(./img/agua6.jpg)'
-  img1.style.height = "80vh"
+  img.style.marginLeft = '-500%'
 })
 
 change7.addEventListener("click", () => {
   document.body.style.backgroundColor = "#80ffdf"
-  img1.style.backgroundImage = 'url(./img/creative7.jpg)'
-  img1.style.height = "80vh"
+  img.style.marginLeft = '-600%'
 })
 
 change8.addEventListener("click", () => {
   document.body.style.backgroundColor = "#ff80ff"
-  img1.style.backgroundImage = 'url(./img/fight8.jpg)'
-  img1.style.height = "80vh"
+  img.style.marginLeft = '-700%'
 })
 
 change9.addEventListener("click", () => {
   document.body.style.backgroundColor = "#99e699"
-  img1.style.backgroundImage = 'url(./img/folha9.png)'
-  img1.style.height = "80vh"
+  img.style.marginLeft = '-800%'
 })
 
 change10.addEventListener("click", () => {
   document.body.style.backgroundColor = "#df9f9f"
-  img1.style.backgroundImage = 'url(./img/thank10.jpg)'
-  img1.style.height = "80vh"
+  img.style.marginLeft = '-900%'
 })
 
